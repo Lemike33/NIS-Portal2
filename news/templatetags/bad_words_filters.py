1e371771387f1353a8f56a340f98a52e2bdbd10a
Lemike33 <leshukovv87@mail.ru>
2023-01-11 16:03:44 +0300

    HW module D6 to check for mentor

---
+++ news/templatetags/bad_words_filters.py
@@ -0,0 +1,28 @@
+import re
+from django import template
+from ..resourses import BAD
+register = template.Library()
+
+
+@register.filter()
+def bad_words(value):
+    text_list = re.split(r'[,;!).? ]+', value)
+    clean_list = []
+    for el in text_list:
+        if (el.lower() or el.title()) in BAD:
+            el = el[0] + ((len(el) - 1) * '*')
+            clean_list.append(el)
+        else:
+            clean_list.append(el)
+    clean_text = " ".join(clean_list)
+    value = clean_text
+    return f'{value}'
+
+
+
+
+
+
+
+
+
--- news/views.py
+++ news/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from datetime import datetime
+from .models import Post
 
-# Create your views here.
+
+class ShowNewsView(ListView):
+    model = Post  # Указываем модель, объекты которой мы будем выводить
+    ordering = ['-date']  # Поле, которое будет использоваться для сортировки объектов
+    template_name = 'news/showNews.html'  # Указываем имя шаблона, в котором будут все инструкции о том, как именно пользователю должны быть показаны наши объекты
+    context_object_name = 'news'  # Это имя списка, в котором будут лежать все объекты в HTML обращаемся к нему!!!
+    # paginate_by = 2  # постраничный вывод, сколько объектов выводим на 1 страницу
+
+    # Метод get_context_data позволяет нам изменить набор данных,
+    # который будет передан в шаблон.
+    def get_context_data(self, **kwargs):
+        # С помощью super() мы обращаемся к родительским классам
+        # и вызываем у них метод get_context_data с теми же аргументами,
+        # что и были переданы нам.
+        # В ответе мы должны получить словарь.
+        context = super().get_context_data(**kwargs)
+        # К словарю добавим текущую дату в ключ 'time_now'.
+        context['time_now'] = datetime.utcnow()
+        # Добавим ещё одну пустую переменную,
+        # чтобы на её примере рассмотреть работу ещё одного фильтра.
+        context['next_news'] = 'Ожидайте новые новости'
+        return context
+
+
+class NewsDetailView(DetailView):
+    model = Post
+    template_name = 'news/showNewsDetail.html'
+    context_object_name = 'news'
+
+
